3793d4569deeb0457fa54d53462093549bb1f60c
Michael Bryan <michaelfbryan@gmail.com>
2019-08-25 18:24:47 +0800

    Wired up caps negotiation and the rgb-to-grayscale transform on video frames

--- my-first-filter/src/rgb_2_gray.rs
+++ my-first-filter/src/rgb_2_gray.rs
@@ -4,14 +4,17 @@ use glib::{
 };
 use gstreamer::{
     subclass::{prelude::*, ElementInstanceStruct},
-    Caps, DebugCategory, DebugColorFlags, Element, Fraction, FractionRange,
-    IntRange, List, PadDirection, PadPresence, PadTemplate, Plugin, Rank,
+    Buffer, BufferRef, Caps, CapsIntersectMode, CoreError, DebugCategory,
+    DebugColorFlags, Element, ErrorMessage, FlowError, FlowSuccess, Fraction,
+    FractionRange, IntRange, List, PadDirection, PadPresence, PadTemplate,
+    Plugin, Rank,
 };
 use gstreamer_base::{
     subclass::{prelude::*, BaseTransformMode},
     BaseTransform,
 };
-use gstreamer_video::VideoFormat;
+use gstreamer_video::{VideoFormat, VideoFrameRef, VideoInfo};
+use std::sync::Mutex;
 
 pub fn register(plugin: &Plugin) -> Result<(), BoolError> {
     Element::register(
@@ -23,8 +26,25 @@ pub fn register(plugin: &Plugin) -> Result<(), BoolError> {
 }
 
 pub struct Rgb2Gray {
-    #[allow(dead_code)]
     cat: DebugCategory,
+    state: Mutex<Option<State>>,
+}
+
+fn bgrx_to_gray(in_p: &[u8]) -> u8 {
+    // See https://en.wikipedia.org/wiki/YUV#SDTV_with_BT.601
+    const R_Y: u32 = 19595; // 0.299 * 65536
+    const G_Y: u32 = 38470; // 0.587 * 65536
+    const B_Y: u32 = 7471; // 0.114 * 65536
+
+    assert_eq!(in_p.len(), 4);
+
+    let b = u32::from(in_p[0]);
+    let g = u32::from(in_p[1]);
+    let r = u32::from(in_p[2]);
+
+    let gray = ((r * R_Y) + (g * G_Y) + (b * B_Y)) / 65536;
+
+    (gray as u8)
 }
 
 impl ObjectSubclass for Rgb2Gray {
@@ -43,6 +63,7 @@ impl ObjectSubclass for Rgb2Gray {
                 DebugColorFlags::empty(),
                 Some("Rust RGB-GRAY converter"),
             ),
+            state: Mutex::new(None),
         }
     }
 
@@ -116,4 +137,220 @@ impl ObjectImpl for Rgb2Gray {
     glib_object_impl!();
 }
 impl ElementImpl for Rgb2Gray {}
-impl BaseTransformImpl for Rgb2Gray {}
+impl BaseTransformImpl for Rgb2Gray {
+    fn set_caps(
+        &self,
+        element: &BaseTransform,
+        incaps: &Caps,
+        outcaps: &Caps,
+    ) -> bool {
+        let in_info = match VideoInfo::from_caps(incaps) {
+            None => return false,
+            Some(info) => info,
+        };
+        let out_info = match VideoInfo::from_caps(outcaps) {
+            None => return false,
+            Some(info) => info,
+        };
+
+        gstreamer::gst_debug!(
+            self.cat,
+            obj: element,
+            "Configured for caps {} to {}",
+            incaps,
+            outcaps
+        );
+
+        *self.state.lock().unwrap() = Some(State { in_info, out_info });
+
+        true
+    }
+
+    fn stop(&self, element: &BaseTransform) -> Result<(), ErrorMessage> {
+        // Drop state
+        let _ = self.state.lock().unwrap().take();
+
+        gstreamer::gst_info!(self.cat, obj: element, "Stopped");
+
+        Ok(())
+    }
+
+    fn get_unit_size(
+        &self,
+        _element: &BaseTransform,
+        caps: &Caps,
+    ) -> Option<usize> {
+        VideoInfo::from_caps(caps).map(|info| info.size())
+    }
+
+    fn transform_caps(
+        &self,
+        element: &BaseTransform,
+        direction: PadDirection,
+        caps: &Caps,
+        filter: Option<&Caps>,
+    ) -> Option<Caps> {
+        let other_caps = if direction == PadDirection::Src {
+            let mut caps = caps.clone();
+
+            for s in caps.make_mut().iter_mut() {
+                s.set("format", &VideoFormat::Bgrx.to_string());
+            }
+
+            caps
+        } else {
+            let mut gray_caps = Caps::new_empty();
+
+            {
+                let gray_caps = gray_caps.get_mut().unwrap();
+
+                for s in caps.iter() {
+                    let mut s_gray = s.to_owned();
+                    s_gray.set("format", &VideoFormat::Gray8.to_string());
+                    gray_caps.append_structure(s_gray);
+                }
+                gray_caps.append(caps.clone());
+            }
+
+            gray_caps
+        };
+
+        gst_debug!(
+            self.cat,
+            obj: element,
+            "Transformed caps from {} to {} in direction {:?}",
+            caps,
+            other_caps,
+            direction
+        );
+
+        if let Some(filter) = filter {
+            Some(
+                filter
+                    .intersect_with_mode(&other_caps, CapsIntersectMode::First),
+            )
+        } else {
+            Some(other_caps)
+        }
+    }
+
+    fn transform(
+        &self,
+        element: &BaseTransform,
+        inbuf: &Buffer,
+        outbuf: &mut BufferRef,
+    ) -> Result<FlowSuccess, FlowError> {
+        // lock the state and make sure we've started
+        let mut state_guard = self.state.lock().unwrap();
+        let state = state_guard.as_mut().ok_or_else(|| {
+            gst_element_error!(
+                element,
+                CoreError::Negotiation,
+                ["Have no state yet"]
+            );
+            FlowError::NotNegotiated
+        })?;
+
+        // make sure the incoming buffer is readable
+        let in_frame = VideoFrameRef::from_buffer_ref_readable(
+            inbuf.as_ref(),
+            &state.in_info,
+        )
+        .ok_or_else(|| {
+            gst_element_error!(
+                element,
+                CoreError::Failed,
+                ["Failed to map input buffer readable"]
+            );
+            FlowError::Error
+        })?;
+
+        // make sure the outgoing buffer is writeable
+        let mut out_frame =
+            VideoFrameRef::from_buffer_ref_writable(outbuf, &state.out_info)
+                .ok_or_else(|| {
+                    gst_element_error!(
+                        element,
+                        CoreError::Failed,
+                        ["Failed to map output buffer writable"]
+                    );
+                    FlowError::Error
+                })?;
+
+        let width = in_frame.width() as usize;
+        let in_stride = in_frame.plane_stride()[0] as usize;
+        let in_data = in_frame.plane_data(0).unwrap();
+        let out_stride = out_frame.plane_stride()[0] as usize;
+        let out_format = out_frame.format();
+        let out_data = out_frame.plane_data_mut(0).unwrap();
+
+        if out_format == VideoFormat::Bgrx {
+            // the sink asked for Bgrx (it doesn't support Gray8 :/)
+
+            // sanity checks
+            assert_eq!(in_data.len() % 4, 0);
+            assert_eq!(out_data.len() % 4, 0);
+            assert_eq!(out_data.len() / out_stride, in_data.len() / in_stride);
+
+            let in_line_bytes = width * 4;
+            let out_line_bytes = width * 4;
+
+            assert!(in_line_bytes <= in_stride);
+            assert!(out_line_bytes <= out_stride);
+
+            // walk over the pixel rows and columns
+            for (in_line, out_line) in in_data
+                .chunks_exact(in_stride)
+                .zip(out_data.chunks_exact_mut(out_stride))
+            {
+                for (in_p, out_p) in in_line[..in_line_bytes]
+                    .chunks_exact(4)
+                    .zip(out_line[..out_line_bytes].chunks_exact_mut(4))
+                {
+                    assert_eq!(out_p.len(), 4);
+
+                    // copy the grayscale version to the output buffer
+                    let gray = bgrx_to_gray(in_p);
+                    out_p[0] = gray;
+                    out_p[1] = gray;
+                    out_p[2] = gray;
+                }
+            }
+        } else if out_format == VideoFormat::Gray8 {
+            // sanity checks
+            assert_eq!(in_data.len() % 4, 0);
+            assert_eq!(out_data.len() / out_stride, in_data.len() / in_stride);
+
+            let in_line_bytes = width * 4;
+            let out_line_bytes = width;
+
+            assert!(in_line_bytes <= in_stride);
+            assert!(out_line_bytes <= out_stride);
+
+            // walk over the pixel rows and columns, setting the new pixel value
+            // to its grayscale version
+            for (in_line, out_line) in in_data
+                .chunks_exact(in_stride)
+                .zip(out_data.chunks_exact_mut(out_stride))
+            {
+                for (in_p, out_p) in in_line[..in_line_bytes]
+                    .chunks_exact(4)
+                    .zip(out_line[..out_line_bytes].iter_mut())
+                {
+                    let gray = bgrx_to_gray(in_p);
+                    *out_p = gray;
+                }
+            }
+        } else {
+            // what output format is this?!
+            unimplemented!();
+        }
+
+        Ok(FlowSuccess::Ok)
+    }
+}
+
+struct State {
+    in_info: VideoInfo,
+    out_info: VideoInfo,
+}
